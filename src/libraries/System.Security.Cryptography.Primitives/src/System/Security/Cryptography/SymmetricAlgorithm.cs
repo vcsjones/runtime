3fc38b37120f188ea0ee3b27b7cb1547f02bb6ea
Kevin Jones <kevin@vcsjones.com>
2019-11-27 11:24:55 -0500

    Add GetCiphertextLength

--- src/libraries/System.Security.Cryptography.Primitives/src/System/Security/Cryptography/SymmetricAlgorithm.cs
+++ src/libraries/System.Security.Cryptography.Primitives/src/System/Security/Cryptography/SymmetricAlgorithm.cs
@@ -8,7 +8,7 @@ using Internal.Cryptography;
 
 namespace System.Security.Cryptography
 {
-    public abstract class SymmetricAlgorithm : IDisposable
+    public abstract partial class SymmetricAlgorithm : IDisposable
     {
         protected SymmetricAlgorithm()
         {
@@ -220,6 +220,39 @@ namespace System.Security.Cryptography
             if (validSizes == null)
                 return false;
             return bitLength.IsLegalSize(validSizes);
+        }
+
+        public int GetCiphertextLength(PaddingMode paddingMode, int plaintextLength)
+        {
+            if (plaintextLength < 0)
+                throw new ArgumentOutOfRangeException(nameof(plaintextLength), SR.ArgumentOutOfRange_NeedNonNegNum);
+
+            if (BlockSize <= 0 || (BlockSize & 0b111) != 0)
+                throw new CryptographicException(SR.Cryptography_UnsupportedBlockSize);
+
+            int blockSizeBytes = BlockSize / 8;
+            int remainder = plaintextLength % blockSizeBytes;
+
+            switch (paddingMode)
+            {
+                case PaddingMode.None when (remainder != 0):
+                    throw new ArgumentOutOfRangeException(nameof(plaintextLength), SR.Cryptography_MatchBlockSize);
+                case PaddingMode.None:
+                case PaddingMode.Zeros when (remainder == 0):
+                    return plaintextLength;
+                case PaddingMode.Zeros:
+                case PaddingMode.PKCS7:
+                case PaddingMode.ANSIX923:
+                case PaddingMode.ISO10126:
+                    int nearestWholeBlock = plaintextLength - remainder;
+
+                    if (int.MaxValue - nearestWholeBlock < blockSizeBytes)
+                        throw new CryptographicException(SR.Cryptography_PlaintextTooLarge);
+
+                    return nearestWholeBlock + blockSizeBytes;
+                default:
+                    throw new ArgumentException(SR.Cryptography_InvalidPaddingMode, nameof(paddingMode));
+            }
         }
 
         protected CipherMode ModeValue;
